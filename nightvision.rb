509196534d82092a089bd7d033faadf0fdabe27f
Aidan Steele <aidan@nimblersecurity.io>
2024-12-31 11:54:51 -0600

    Brew formula update for nightvision version 0.7.4

--- nightvision.rb
+++ nightvision.rb
@@ -10,7 +10,7 @@ class Nightvision < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://downloads.nightvision.net/binaries/0.7.4/nightvision_0.7.4_darwin_amd64.tar.gz"
-      sha256 "c857128fe13586bc80a89d770343ae367512b06a8306ee1efc2f74eced44d89c"
+      sha256 "f287fc5cffd955c2e4dfc5179df246b58165a921a6c6fb828c675bc2284be966"
 
       def install
         bin.install "nightvision"
@@ -18,7 +18,7 @@ class Nightvision < Formula
     end
     if Hardware::CPU.arm?
       url "https://downloads.nightvision.net/binaries/0.7.4/nightvision_0.7.4_darwin_arm64.tar.gz"
-      sha256 "14a5e3bb12df17313b1c214ffe96363e4841ec17f70af94db52a2eeb179b76b2"
+      sha256 "c12630c5418a0e2991637b20eef79e5231e3f3fb5f172d05a7d2fbe1eb0d9fe8"
 
       def install
         bin.install "nightvision"
@@ -30,7 +30,7 @@ class Nightvision < Formula
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://downloads.nightvision.net/binaries/0.7.4/nightvision_0.7.4_linux_amd64.tar.gz"
-        sha256 "051c125f98548592a4fd22453466aed5da80eb47dcc307f99f6ed43c2dc88501"
+        sha256 "11c85849e7bfcfe681fe638aefc210fa1884a3de2324313ced8189f0e6d997e9"
 
         def install
           bin.install "nightvision"
@@ -40,7 +40,7 @@ class Nightvision < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://downloads.nightvision.net/binaries/0.7.4/nightvision_0.7.4_linux_arm64.tar.gz"
-        sha256 "9d12579ae9987436db7ab6f9cffcee86602855cc3feed43b24ca7daf376bdfa3"
+        sha256 "09eaaadf5df8446c9499e69ed9eacd3c713c65e7d34b4bafc5fcf4dffe3e4e81"
 
         def install
           bin.install "nightvision"
